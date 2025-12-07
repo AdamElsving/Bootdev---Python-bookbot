dfcf6dff4fe3325e6488efb2588e6060c6a080a7
AdamElsving <adamheiseelswing@gmail.com>
2025-12-07 01:10:10 +0100

    Program now takes book path as argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_count_characters, get_sorted_characters
+import sys
 
 def get_book_text(file_path:str):
 
@@ -8,7 +9,11 @@ def get_book_text(file_path:str):
     return file_contents
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)
+
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
 
     word_count = get_num_words(book_text)
